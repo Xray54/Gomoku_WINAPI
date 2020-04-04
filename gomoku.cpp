378b376b61f66c658e2674d7c892e95ca32d47f2
원식 SEONG <leo258258@naver.com>
2020-04-04 13:50:35 +0900

    Make Gomoku Frame

--- gomoku.cpp
+++ gomoku.cpp
@@ -1,1 +1,60 @@
-﻿
+﻿#include "gomoku.h"
+
+Gomoku::Gomoku(int board_length)
+	: board_length_{ board_length },
+	turn_{ black },
+	board_{ board_length * board_length,none },
+	last_move_{ -1,-1 }
+{
+	
+}
+
+bool Gomoku::PieceOnBoard(int x, int y)
+{
+	//check empty and set
+	if (board_[x + y * board_length_] == none) {
+		board_[x + y * board_length_] == turn_;
+
+		//record last move to CheckWin()
+		last_move_ = { x,y };
+
+		return true;
+	}
+	return false;
+}
+
+bool Gomoku::CheckWin() {
+	int x{ last_move_.x };
+	int y{ last_move_.y };
+	int connected_piece{ 1 };
+
+	//check vertical 
+	for (int i = 1; board(x, y-i) == turn_; i++) 
+		connected_piece++;
+	for (int i = 1; board(x, y+i) == turn_; i++) 
+		connected_piece++;
+	
+	if (connected_piece >= 5)
+		return true;
+
+	//check diagonal
+	connected_piece = 1;
+	for (int i = 1; board(x+i, y - i) == turn_; i++) 
+		connected_piece++;
+	for (int i = 1; board(x-i, y + i) == turn_; i++) 
+		connected_piece++;
+	
+	if (connected_piece >= 5)
+		return true;
+
+	//check horizontal
+	for (int i = 1; board(x+i, y ) == turn_; i++)
+		connected_piece++;
+	for (int i = 1; board(x-i, y ) == turn_; i++)
+		connected_piece++;
+
+	if (connected_piece >= 5)
+		return true;
+
+	return false;
+}
--- gomoku.h
+++ gomoku.h
@@ -3,9 +3,46 @@
 #include <vector>
 using namespace std;
 
-class gomoku {
+struct Point {
+	int x;
+	int y;
+};
+
+class Gomoku {
 private:
-	vector<char> borard;
+	enum piece{none,black,white};
+	vector<piece> board_;
+	piece board(int x, int y) {
+		if (x < 0 || board_length_ < x ||
+			y < 0 || board_length_ < y) {
+			return none;
+		}
+		return board_[x + y * board_length_ ];
+	}
+	piece turn_;
+	int board_length_;
+
+	Point last_move_;
+	
 public:
-	gomoku();
+	Gomoku(int board_length = 19);
+
+	//order
+	//PieceOnBoard -> CheckWin -> PassTurn
+
+	// return true when success
+	bool PieceOnBoard(int x,int y);
+
+	//Check base on last move
+	bool CheckWin();
+
+	//pass turn
+	void PassTurn() {
+		if (turn_ == black)
+			turn_ = white;
+		else
+			turn_ = black;
+	}
+	
+	piece getturn() { return turn_; }
 };
